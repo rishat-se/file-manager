47f3a86ba1b1fd4eeb756c18c9f9f760e9858add
Rishat Agzamov <rishat.se@gmail.com>
2022-12-15 16:24:04 -0500

    fix: change import for fs lib

--- src/fs/change-dir.js
+++ src/fs/change-dir.js
@@ -1,13 +1,12 @@
 import * as path from 'node:path';
-import { stat } from 'node:fs/promises';
+import * as fs from 'node:fs/promises';
 
 const changeDir = async (workDir, cmdArgs) => {
     if (cmdArgs.length !== 1) throw new Error('Invalid Input');
     let newPath = path.resolve(workDir, cmdArgs[0]);
     if (cmdArgs[0].match(/^[A-Za-z]:$/)) newPath = path.join(cmdArgs[0], path.sep);
-    //    console.log(newPath);
     try {
-        const fstat = await stat(newPath);
+        const fstat = await fs.stat(newPath);
         if (!fstat.isDirectory()) throw new Error();
         return newPath;
     } catch {
